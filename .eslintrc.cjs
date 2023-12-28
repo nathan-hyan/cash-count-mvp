0c04b1dd1d5a8a5bb187f97015979e07e252ccfd
Aldo Exequiel Mleziva <aldo.exequiel.mleziva@hp.com>
2023-12-28 13:57:53 -0300

    build: :wrench: Removed eslint spread op rule

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,57 +4,58 @@ module.exports = {
     es2021: true,
   },
   extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:react/recommended",
-    "airbnb",
-    "airbnb-typescript",
-    "airbnb/hooks",
-    "plugin:prettier/recommended",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'airbnb',
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:prettier/recommended',
   ],
   overrides: [
     {
       env: {
         node: true,
       },
-      files: [".eslintrc.{js,cjs}"],
+      files: ['.eslintrc.{js,cjs}'],
       parserOptions: {
-        sourceType: "script",
+        sourceType: 'script',
       },
     },
     {
-      files: ["*.js", "*.jsx", "*.ts", "*.tsx"],
+      files: ['*.js', '*.jsx', '*.ts', '*.tsx'],
       rules: {
-        "simple-import-sort/imports": [
-          "error",
+        'simple-import-sort/imports': [
+          'error',
           {
             groups: [
               // Packages `react` related packages come first.
-              ["^react", "^@?\\w"],
+              ['^react', '^@?\\w'],
               // Internal packages.
-              ["^(@)(/.*|$)"],
+              ['^(@)(/.*|$)'],
               // Absolute imports, starting with ~
-              ["^~.*(?:/|$)"],
+              ['^~.*(?:/|$)'],
               // Parent imports. Put `..` last.
               [
                 "^\\./(?=.*/)(?!/?$)', '^\\.(?!/?$)', '^\\./?$', '^\\.\\.(?!/?$)",
               ],
               // Style imports.
-              ["^.+\\.?(scss|css)?inline$", "^.+\\.?(scss|css)$"],
+              ['^.+\\.?(scss|css)?inline$', '^.+\\.?(scss|css)$'],
             ],
           },
         ],
       },
     },
   ],
-  parser: "@typescript-eslint/parser",
+  parser: '@typescript-eslint/parser',
   parserOptions: {
-    ecmaVersion: "latest",
-    sourceType: "module",
-    project: ["./tsconfig.json"],
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: ['./tsconfig.json'],
   },
-  plugins: ["@typescript-eslint", "react", "simple-import-sort", "prettier"],
+  plugins: ['@typescript-eslint', 'react', 'simple-import-sort', 'prettier'],
   rules: {
-    "react/react-in-jsx-scope": "off",
+    'react/react-in-jsx-scope': 'off',
+    'react/jsx-props-no-spreading': 'off',
   },
 };
--- src/Components/Form.tsx
+++ src/Components/Form.tsx
@@ -2,21 +2,13 @@ import { useForm } from 'react-hook-form';
 import Typography from '@mui/material/Typography';
 
 export default function Form() {
-  const form = useForm();
-  const { register } = form;
-  const { name, ref, onChange, onBlur } = register('username');
+  const { register } = useForm();
+
   return (
     <form>
       <label htmlFor='username'>
         <Typography>Username</Typography>
-        <input
-          type='text'
-          id='username'
-          name={name}
-          ref={ref}
-          onChange={onChange}
-          onBlur={onBlur}
-        />
+        <input {...register('username')} type='text' id='username' />
       </label>
     </form>
   );
