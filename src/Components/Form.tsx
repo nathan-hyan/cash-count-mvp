eb77b8d17c0473787bb7f9c699e4cac4f399f3a0
AndresEstebanGonzalez <andresandye@gmail.com>
2023-12-28 12:42:46 -0300

    feat: :sparkles: Integrar react hook form.

---
+++ src/Components/Form.tsx
@@ -0,0 +1,7 @@
+import { useForm } from 'react-hook-form';
+import Typography from '@mui/material/Typography';
+
+export default function Form() {
+  const form = useForm();
+  return <Typography>FORM</Typography>;
+}
