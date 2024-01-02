a9d4b75ec527593438be62440ccea5105f19a397
AndresEstebanGonzalez <andresandye@gmail.com>
2024-01-02 19:52:52 -0300

    fix: :bug: Navbar problems fixed.

--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,37 +1,37 @@
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
+import { grey } from '@mui/material/colors';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 
 export default function Navbar() {
   return (
     <Box sx={{ mt: 1.5 }}>
-      <AppBar
-        position='static'
-        sx={{
-          boxShadow: 'none',
-        }}
-      >
+      <AppBar position='static' elevation={0}>
         <Toolbar
           sx={{
             height: '3.5rem',
-            color: '#000',
-            backgroundColor: '#D9D9D9',
+            color: grey[900],
+            bgcolor: grey[300],
           }}
         >
           <Typography
             variant='h6'
-            component='div'
+            component='h1'
             sx={{ flexGrow: 1, ml: '5rem', fontSize: '1.875rem' }}
           >
             Cash Count
           </Typography>
           <Box sx={{ display: 'flex', width: '13.3rem', fontSize: '1.25rem' }}>
             <Box sx={{ padding: '1.25rem 1rem' }}>
-              <Typography>Dolar Blue</Typography>
+              <Typography variant='subtitle1' component='h2'>
+                Dolar Blue
+              </Typography>
             </Box>
             <Box sx={{ padding: '1.25rem 1rem' }}>
-              <Typography>$975</Typography>
+              <Typography variant='subtitle1' component='h3'>
+                $975
+              </Typography>
             </Box>
           </Box>
         </Toolbar>
