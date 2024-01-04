354fa3491c6f01527f15a0625873d06bc5f8c26c
AndresEstebanGonzalez <andresandye@gmail.com>
2024-01-04 19:19:28 -0300

    feat: :sparkles: Ambito API connected.
    Ambito API is now connected and show the dolar blue value. The value automatically refreshes every 5 minutes.

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { useQuery } from 'react-query';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import { grey } from '@mui/material/colors';
@@ -7,6 +8,28 @@ import Typography from '@mui/material/Typography';
 import PriceDisplay from './PriceDisplay';
 
 export default function Navbar() {
+  const DOLAR_REFETCH_INTERVAL = 300000;
+  const fetchDolarValue = async () => {
+    const response = await fetch('https://mercados.ambito.com/home/general3');
+    return response.json();
+  };
+
+  const { data, status } = useQuery({
+    queryKey: ['dolarValue'],
+    queryFn: fetchDolarValue,
+    refetchInterval: DOLAR_REFETCH_INTERVAL,
+  });
+
+  if (status === 'loading') {
+    return <div>Loading...</div>;
+  }
+
+  if (status === 'error') {
+    return <div>Error: Failed to fetch Dolar value.</div>;
+  }
+
+  const DOLAR_INFORMAL = data[1].venta;
+
   return (
     <Box sx={{ mt: 1.5 }}>
       <AppBar position='static' elevation={0}>
@@ -20,8 +43,7 @@ export default function Navbar() {
           <Typography variant='h1' sx={{ fontSize: '1.6rem', flexGrow: 1 }}>
             Cash Count
           </Typography>
-
-          <PriceDisplay label='Dolar Blue' amount={999} />
+          <PriceDisplay label='Dolar Blue' amount={DOLAR_INFORMAL} />
         </Toolbar>
       </AppBar>
     </Box>
